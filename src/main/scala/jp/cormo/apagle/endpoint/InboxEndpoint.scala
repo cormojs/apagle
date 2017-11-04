feefea08a304d26adfa479332a8f6df88ce1d368
cormojs <cormorant2+github@gmail.com>
2017-11-05 00:26:14 +0900

    Generated Tables.scala, DB model classes.

---
+++ src/main/scala/jp/cormo/apagle/endpoint/InboxEndpoint.scala
@@ -0,0 +1,8 @@
+package jp.cormo.apagle.endpoint
+
+import io.finch._
+import jp.cormo.apagle.model.json._
+
+// object InboxEndpoint extends Endpoint[activitypub.Inbox] {
+
+// }
---
+++ src/main/scala/jp/cormo/apagle/model/json/activitypub/Inbox.scala
@@ -0,0 +1,7 @@
+package jp.cormo.apagle.model.json.activitypub
+
+import jp.cormo.apagle.model.Account
+import jp.cormo.apagle.model.json._
+
+// class Inbox(val a: Account) extends activitystreams.Object("inbox") {
+// }
